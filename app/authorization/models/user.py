59b4495959b821e2de56c3404f8c2e50494336ea
Vadim Sokoltsov <vforvad@gmail.com>
2017-06-25 01:59:37 +0300

    add base user model and initial migration

--- app/authorization/models/user.py
+++ app/authorization/models/user.py
@@ -1,4 +1,20 @@
-from . import AbstractBaseUser
+from . import AbstractBaseUser, BaseUserManager
+from . import models
 
 class User(AbstractBaseUser):
-    pass
+    """ Represents a user object in our system """
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = BaseUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        """ String representation of user """
+
+        return self.email
