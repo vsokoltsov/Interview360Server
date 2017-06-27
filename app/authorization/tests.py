c3c8e8f477a5904648baec371a41c54d623192ff
Vadim Sokoltsov <vforvad@gmail.com>
2017-06-27 15:55:29 +0300

    add succesfull test

--- app/authorization/tests.py
+++ app/authorization/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from rest_framework.test import APITestCase
 # Create your tests here.
+import ipdb
+
+class RegistrationViewTests(APITestCase):
+
+    def test_success_sign_up(self):
+        response = self.client.post('/api/v1/sign_up/', {'email': 'example@mail.com', 'password': '12345678'})
+        self.assertEqual('token' in response.data, True)
