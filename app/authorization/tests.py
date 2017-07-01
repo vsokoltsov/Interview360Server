f0a24e8807a09c1c92cb41851915838e84b89f47
Vadim Sokoltsov <vforvad@gmail.com>
2017-07-02 02:26:11 +0300

    add tests for success and failure token generation

--- app/authorization/tests.py
+++ app/authorization/tests.py
@@ -91,3 +91,27 @@ class AuthorizationFormTests(TestCase):
         form_data = { 'email': 'example@mail.com', 'password': '' }
         form = AuthorizationForm(form_data)
         self.assertEqual(form.submit(), False)
+
+    def test_form_success_submit_create_token(self):
+        """ Test that token is generated after success form submit """
+
+        user = User.objects.create(email="example@mail.com")
+        user.set_password('12345678')
+        user.save()
+
+        form_data = { 'email': 'example@mail.com', 'password': '12345678' }
+        form = AuthorizationForm(form_data)
+        form.submit()
+        self.assertIsNotNone(form.token)
+
+    def test_form_failed_submit_token_is_abscent(self):
+        """ Test that token is not generated if form is not submited correctly """
+
+        user = User.objects.create(email="example@mail.com")
+        user.set_password('12345678')
+        user.save()
+
+        form_data = { 'email': 'example@mail.com', 'password': '' }
+        form = AuthorizationForm(form_data)
+        form.submit()
+        self.assertEqual(hasattr(form, 'token'), False)
