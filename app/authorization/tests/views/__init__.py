0816469462e5df58cfeeb62975517e18c411727a
Vadim Sokoltsov <vforvad@gmail.com>
2017-07-04 14:05:34 +0300

    add tests for current_user view

--- app/authorization/tests/views/__init__.py
+++ app/authorization/tests/views/__init__.py
@@ -3,3 +3,4 @@ from authorization.models import User
 
 from .authorization import AuthorizationViewSetTests
 from .registration import RegistrationViewTests
+from .current_user import CurrentUserViewTests
---
+++ app/authorization/tests/views/current_user.py
@@ -0,0 +1,23 @@
+from . import APITestCase, User
+from rest_framework.authtoken.models import Token
+import ipdb
+
+class CurrentUserViewTests(APITestCase):
+    """ Tests for current user receiving methods """
+    def setUp(self):
+        user = User.objects.create(email="example@mail.com", password="12345678")
+        self.token = Token.objects.create(user=user)
+
+    def test_success_user_receiving(self):
+        """ Test getting current user info if token is present """
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.get('/api/v1/current/')
+        self.assertEqual('current_user' in response.data, True)
+
+    def test_failed_user_receiving(self):
+        """ Test failed attempt of getting user if token is abscent """
+        
+        response = self.client.get('/api/v1/current/')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual('detail' in response.data, True)
