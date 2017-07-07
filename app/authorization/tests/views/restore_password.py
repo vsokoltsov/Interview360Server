90d697a3f0861a61be0f356176da828903a5f04a
Vadim Sokoltsov <vforvad@gmail.com>
2017-07-07 15:51:37 +0300

    add left tests for user restore password flow

--- app/authorization/tests/views/restore_password.py
+++ app/authorization/tests/views/restore_password.py
@@ -3,15 +3,37 @@ from . import APITestCase, User
 class ResstorePasswordViewTest(APITestCase):
     """ Test of RestorePassword view class """
 
+    def setUp(self):
+        """ Setting up test credentials """
+
+        self.user = User.objects.create(email="example@mail.com")
+        self.user.set_password('12345678')
+        self.user.save()
+
     def test_success_restore_password(self):
         """ Test success workflow of restoring user password """
-
-        user = User.objects.create(email="example@mail.com")
-        user.set_password('12345678')
-        user.save()
 
         response = self.client.post('/api/v1/restore_password/',  {
                 'email': 'example@mail.com'
         })
 
         self.assertEqual('message' in response.data, True)
+
+    def test_failed_restore_password(self):
+        """ Tests failed workflow for restoring user's password """
+
+        response = self.client.post('/api/v1/restore_password/',  {
+                'email': ''
+        })
+
+        self.assertEqual('errors' in response.data, True)
+
+    def test_failed_restore_password_if_user_does_not_exists(self):
+        """ Tests failed workflow for restoring user's password if there is
+            no such user """
+
+        response = self.client.post('/api/v1/restore_password/',  {
+                'email': 'example1@mail.com'
+        })
+
+        self.assertEqual('errors' in response.data, True)
