7ce8ff0f9104d4cacf58673b8735e54fcdd23325
Vadim Sokoltsov <vforvad@gmail.com>
2017-07-07 10:42:58 +0300

    add example of email sending

--- app/authorization/forms/restore_password.py
+++ app/authorization/forms/restore_password.py
@@ -1,6 +1,7 @@
 from . import forms, User
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 class RestorePasswordForm(forms.Form):
     """ Send mail to user with instructions how to reset his password """
@@ -9,10 +10,13 @@ class RestorePasswordForm(forms.Form):
     def submit(self):
         if not self.is_valid(): return False
 
-
         try:
             user = User.objects.get(email=self['email'].value())
             auth_token, _ = Token.objects.get_or_create(user=user)
+            render_to_string('reset_password.html',
+                             { 'reset_link_url': 'http://mail.ru',
+                              'token': '12345' }
+                             )
             return True
         except ObjectDoesNotExist:
             return False
