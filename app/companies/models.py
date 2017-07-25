dda28bf45dfdcf1de73b10cc96b60fac7160b8c0
Vadim Sokoltsov <vforvad@gmail.com>
2017-07-26 00:52:24 +0300

    add CompanyMember model and migration

--- app/companies/models.py
+++ app/companies/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from authorization.models import User
 # Create your models here.
 
 class Company(models.Model):
@@ -7,5 +7,17 @@ class Company(models.Model):
     start_date = models.DateField(null=False)
     description = models.TextField()
     city = models.CharField(null=False, max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CompanyMember(models.Model):
+    ROLES = [
+        'owner',
+        'hr',
+        'emplotee'
+    ]
+    user = models.ForeignKey(User)
+    company = models.ForeignKey(Company)
+    role = models.CharField(max_length=255, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
