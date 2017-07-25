ae407a0c0b9ebf13022ac782871525ff86b61a9e
Vadim Sokoltsov <vforvad@gmail.com>
2017-07-25 22:47:50 +0300

    create model, migrations and add application to settings.py

--- app/companies/models.py
+++ app/companies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    start_date = models.DateField(null=False)
+    description = models.TextField()
+    city = models.CharField(null=False, max_length=255)
