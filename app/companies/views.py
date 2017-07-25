f0a413bcbb088238ed81c1073f444c66394cb8fa
Vadim Sokoltsov <vforvad@gmail.com>
2017-07-25 23:23:04 +0300

    add current user validation for comapnies views

--- app/companies/views.py
+++ app/companies/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 from .serializers import CompanySerializer
 from .models import Company
 
 
 # Create your views here.
 class CompaniesListViewSet(viewsets.ViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def list(self, request):
         queryset = Company.objects.all()
