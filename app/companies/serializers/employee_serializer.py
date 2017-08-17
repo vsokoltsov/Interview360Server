a8925fcbb09e2a8ce264fadcbd097c0a0dc859c2
Vadim Sokoltsov <vforvad@gmail.com>
2017-08-17 18:49:06 +0300

    add create EmployeeSerializer's action and test for success mail sending

--- app/companies/serializers/employee_serializer.py
+++ app/companies/serializers/employee_serializer.py
@@ -1,8 +1,14 @@
-from . import serializers, User, CompanyMember, transaction
+from . import serializers, User, Company, CompanyMember
 from .company_member_serializer import CompanyMemberSerializer
 from authorization.serializers import UserSerializer
 from django.db.utils import IntegrityError
-import ipdb
+from rest_framework.authtoken.models import Token
+
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+
+import os
 
 class EmployeeSerializer(UserSerializer):
     """ Company employee serializer class """
@@ -26,19 +32,34 @@ class EmployeeSerializer(UserSerializer):
                                                 company_id=company_id)
         return CompanyMemberSerializer(queryset, many=True, read_only=True).data
 
-    def create(self, validated_data):
+    def create(self, data):
         """
         Creates a new employee if they are do not exist;
         Send invintation emails
         """
 
         try:
-            with transaction.atomic():
-                for email in validated_data['emails']:
-                    user = self.find_or_create_user(email)
-                return True
-        except:
+            for email in data['emails']:
+                user = self.find_or_create_user(email)
+                token, _ = Token.objects.get_or_create(user=user)
+                company = Company.objects.get(id=data['company_id'])
+                self.send_invite(user, token, company)
+            return True
+        except IntegrityError:
             return False
+
+    def send_invite(self, user, token, company):
+        link_url = '{}/companies/{}/invite'.format(
+            os.environ['DEFAULT_CLIENT_HOST'],
+            company.id
+        )
+        msg = render_to_string('company_invite.html', {
+                          'company': company,
+                          'link_url': link_url,
+                          'token': token }
+                         )
+        send_mail("Company invite mail", msg,
+                  "Anymail Sender <from@example.com>", [user.email])
 
 
     def find_or_create_user(self, email):
@@ -46,5 +67,4 @@ class EmployeeSerializer(UserSerializer):
             user = User.objects.get(email=email)
         except User.DoesNotExist:
             user = User.objects.create(email=email)
-
         return user
--- app/companies/tests/serializers/employee.py
+++ app/companies/tests/serializers/employee.py
@@ -1,13 +1,35 @@
 from . import (
     TransactionTestCase, serializers, Company, CompanyMember,
-    serializers, User, EmployeeSerializer, datetime, mock
+    serializers, User, EmployeeSerializer, datetime, mock, User
 )
-import ipdb
+import django.core.mail as mail
+from django.test import override_settings
 
 class EmployeeSerializerTest(TransactionTestCase):
+    """ Test employee serializer class """
 
-    def test_success_saving(self):
-        form_data = { 'emails': ['example1@mail.com', 'example2@mail.com'], 'company_id': 1}
+    def setUp(self):
+        """ Test credentials set up """
+        self.user = User.objects.create(email="example@mail.com")
+        self.company = Company.objects.create(name="Test",
+                                         city="Test",
+                                         start_date=datetime.date.today())
+
+    @override_settings(
+        EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend'
+    )
+    def test_success_mail_sending(self):
+        """ Test success mail sending after receivng users and the company """
+
+        form_data = {
+            'emails': [
+                'example!@mail.com',
+                'example!@mail.com',
+                'example!@mail.com'
+            ],
+            'company_id': self.company.id
+        }
         serializer = EmployeeSerializer(data=form_data)
         serializer.is_valid()
         serializer.save()
+        self.assertEqual(len(mail.outbox), 3)
