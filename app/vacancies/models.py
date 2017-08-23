979c9c03d7ec3244deaf6ad02eea9cbe95e4a765
Vadim Sokoltsov <vforvad@gmail.com>
2017-08-23 23:42:30 +0300

    add company field to vacancy

--- app/vacancies/models.py
+++ app/vacancies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from companies.models import Company
 
 # Create your models here.
 class Vacancy(models.Model):
@@ -9,6 +10,7 @@ class Vacancy(models.Model):
     salary = models.DecimalField(max_digits=5, decimal_places=3, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    company = models.ForeignKey('companies.Company', null=False)
 
     class Meta:
         db_table = 'vacancies'
