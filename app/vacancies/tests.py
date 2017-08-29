165583d3a5761e80bb0cc996608ec504f2b0c839
Vadim Sokoltsov <vforvad@gmail.com>
2017-08-29 13:49:44 +0300

    add test for new vacancies serializer

--- app/vacancies/tests.py
+++ app/vacancies/tests.py
@@ -8,7 +8,7 @@ from skills.models import Skill
 import datetime
 from decimal import *
 from .serializers import VacancySerializer
-import ipdb
+import mock
 
 class VacancyViewSetTests(APITestCase):
     """ Tests for VacancyViewSet class """
@@ -118,3 +118,84 @@ class VacancySerializerTest(TransactionTestCase):
 
         serializer = VacancySerializer(data={})
         self.assertFalse(serializer.is_valid())
+
+    def test_failed_skills_are_empty(self):
+        """ Test validation failure if skills is empty """
+
+        form_data = {
+            'title': 'Test',
+            'description': 'Test',
+            'salary': '150.00',
+            'company_id': self.company.id
+        }
+
+        serializer = VacancySerializer(data=form_data)
+        self.assertFalse(serializer.is_valid())
+
+    @mock.patch('vacancies.models.Vacancy.objects.create')
+    def test_vacancy_created(self, vacancy_mock):
+        """ Test success calling 'create' for Vacancy """
+
+        vacancy_mock.objects = mock.MagicMock()
+        vacancy_mock.objects.create = mock.MagicMock()
+
+        serializer = VacancySerializer(data=self.form_data)
+        self.assertTrue(serializer.is_valid())
+
+        serializer.save()
+
+        self.assertTrue(vacancy_mock.called)
+
+    def test_skills_was_setted(self):
+        """ Test success adding of skills to vacancy """
+
+        serializer = VacancySerializer(data=self.form_data)
+        self.assertTrue(serializer.is_valid())
+
+        serializer.save()
+
+        vacancy = Vacancy.objects.last()
+        self.assertEqual(len(vacancy.skills.all()), 1)
+
+    def test_vacancy_was_updated(self):
+        """ Test success updating of the vacancy """
+
+        form_data = {
+            'title': 'Test11',
+            'description': 'Test',
+            'salary': '150.00',
+            'company_id': self.company.id,
+            'skills': [
+                self.skill.id
+            ]
+        }
+
+        serializer = VacancySerializer(self.vacancy, data=form_data,
+                                       partial=True)
+        serializer.is_valid()
+
+        v = serializer.save()
+        self.assertTrue(v.title == form_data['title'])
+
+    def test_number_of_skills_was_changed(self):
+        """ Test that new skills were applly """
+
+        new_skill = Skill.objects.create(name="Python")
+        form_data = {
+            'title': 'Test11',
+            'description': 'Test',
+            'salary': '150.00',
+            'company_id': self.company.id,
+            'skills': [
+                self.skill.id,
+                new_skill.id
+            ]
+        }
+
+        serializer = VacancySerializer(self.vacancy, data=form_data,
+                                       partial=True)
+
+        serializer.is_valid()
+
+        v = serializer.save()
+        self.assertEqual(len(v.skills.all()), 2)
