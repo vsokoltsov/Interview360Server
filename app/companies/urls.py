3e132183212b69f7cd9c5db61272b5622e39046b
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-01 04:04:20 +0300

    add InterviewVieSet class and urls for it

--- app/companies/urls.py
+++ app/companies/urls.py
@@ -5,17 +5,25 @@ from rest_framework_nested import routers
 
 from .views import CompaniesViewSet, EmployeesViewSet, EmployeeActivationView
 from vacancies.views import VacancyViewSet
+from interviews.views import InterviewViewSet
 
 router = routers.SimpleRouter()
 router.register('v1/companies', CompaniesViewSet, base_name='companies')
 
 nested_router = routers.NestedSimpleRouter(router, r'v1/companies', lookup='company')
 nested_router.register('employees', EmployeesViewSet, base_name="company-employees")
-nested_router.register('vacancies', VacancyViewSet, base_name="company-vacancies")
+
+vacancies_router = routers.NestedSimpleRouter(router, r'v1/companies', lookup='company')
+vacancies_router.register('vacancies', VacancyViewSet, base_name="company-vacancies")
+
+interviews_router = routers.NestedSimpleRouter(vacancies_router, r'vacancies', lookup='vacancy')
+interviews_router.register('interviews', InterviewViewSet, base_name="vacancy-interviews")
 
 urlpatterns = [
         url(r'', include(router.urls) ),
         url(r'', include(nested_router.urls) ),
+        url(r'', include(vacancies_router.urls) ),
+        url(r'', include(interviews_router.urls) ),
         url(r'^v1/companies/(?P<company_id>[A-Za-z0-9]*)/activate_member',
             EmployeeActivationView.as_view()),
 ]
--- app/interviews/views.py
+++ app/interviews/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .serializers import InterviewSerializer
 # Create your views here.
+#
+class InterviewViewSet(viewsets.ModelViewSet):
+    """ View class for Interviews """
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
+    serializer_class = InterviewSerializer
+
+    def get_queryset(self):
+        """
+        Return scope of interviews where current user is participated
+        """
+        user = self.request.user
+        return user.interviews.all()
