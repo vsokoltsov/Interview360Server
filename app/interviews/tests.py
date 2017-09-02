cd9e23eae3dc96013e32ea3024a944462fb2768b
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-02 20:55:55 +0300

    add fixtures to tests

--- app/interviews/tests.py
+++ app/interviews/tests.py
@@ -269,16 +269,27 @@ class InterviewEmployeeTest(APITestCase):
     """ Tests for InterviewEmployee view class """
 
     fixtures = [
-        "test_fixtures.yaml"
+        "roles.yaml",
+        "skill.yaml",
+        "user.yaml",
+        "auth_token.yaml",
+        "company.yaml",
+        "vacancy.yaml",
+        "interview.yaml"
     ]
+
+    def setUp(self):
+        """ Setting up test dependencies """
+
+        self.interview = Interview.objects.last()
+        self.employee = InterviewEmployee.objects.get(
+            interview_id=self.interview.id, role_id=4).employee
 
     def test_success_delete_interview_employee(self):
         """ Test success deletion of the interview employee """
 
-        ipdb.set_trace()
-
         url = "/api/v1/interviews/{}/employees/{}/".format(
-            self.interview.id, self.owner.id
+            self.interview.id, self.employee.id
         )
         response = self.client.delete(url)
         self.assertEqual(response.status_code, 204)
