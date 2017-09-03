bb25c1f005ecd1498ee1756bb6fb576c72cde028
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-03 13:53:33 +0300

    add boilerplate for role based model

--- app/roles/models.py
+++ app/roles/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
+ROLE_IDENTIFIERS = {
+    '1': 'AuthorizedUser',
+    '2': 'CompanyOwner',
+    '3': 'Hr',
+    '4': 'Candidate',
+    '5': 'Employee'
+}
+
+def get_user_role_in_company():
+    pass  
+
+class AuthorizedUser:
+    pass
+
+class CompanyOwner:
+    pass
+
+class Hr:
+    pass
+
+class Candidate:
+    pass
+
+class Employee:
+    pass
+
 class Role(models.Model):
     """ User's roles representation """
 
@@ -11,7 +37,7 @@ class Role(models.Model):
     #       permissions level
     #   2 Add boolean field for any particular operation
     #       allowed (or not allowed) to user
-    #   3 Implement STI for roles and create new instances for every 
+    #   3 Implement STI for roles and create new instances for every
     #       company_member instance (same as the 1)
 
     class Meta:
