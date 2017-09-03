dc6b2e7f1f82dfbfa95187c98968110b52355d82
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-03 21:30:22 +0300

    implement some kind of permissions mechanism

--- app/roles/models.py
+++ app/roles/models.py
@@ -1,30 +1,67 @@
 from django.db import models
 
 ROLE_IDENTIFIERS = {
-    '1': 'AuthorizedUser',
-    '2': 'CompanyOwner',
-    '3': 'Hr',
-    '4': 'Candidate',
-    '5': 'Employee'
+    '1': 'CompanyOwner',
+    '2': 'Hr',
+    '3': 'Candidate',
+    '4': 'Employee'
 }
+CREATE_COMPANY = 'create_company'
+UPDATE_COMPANY = 'update_company'
+DELETE_COMPANY = 'delete_company'
+ADD_EMPLOYEE_TO_COMPANY = 'add_employee_to_company'
+RECEIVE_VACANCY = 'receive_vacancy'
+CREATE_VACANCY = 'create_vacancy'
+UPDATE_VACANCY = 'update_vacancy'
+DELETE_VACANCY = 'delete_vacancy'
+ADD_EMPLOYEE_TO_INTERVIEW = 'add_employee_to_interview'
+RECEIVE_INTERVIEW = 'receive_interview'
+CREATE_INTERVIEW = 'create_interview'
+UPDATE_INTERVIEW = 'update_interview'
+DELETE_INTERVIEW = 'delete_interview'
+PARTICIPATE_INTERVIEW = 'participate_interview'
+CREATE_FEEDBACK = 'create_feedback'
+UPDATE_FEEDBACK = 'update_feedback'
+DELETE_FEEDBACK = 'delete_feedback'
 
 def get_user_role_in_company():
-    pass  
-
-class AuthorizedUser:
     pass
 
-class CompanyOwner:
+class RoleManager:
     pass
 
-class Hr:
-    pass
+class Candidate(RoleManager):
+    permissions = [
+        RECEIVE_VACANCY,
+        RECEIVE_INTERVIEW
+    ]
 
-class Candidate:
-    pass
+class Employee(Candidate):
+    permissions = Candidate.permissions + [
+        PARTICIPATE_INTERVIEW,
+        CREATE_FEEDBACK,
+        UPDATE_FEEDBACK,
+        DELETE_FEEDBACK
+    ]
 
-class Employee:
-    pass
+class Hr(Employee):
+    permissions = Employee.permissions + [
+        CREATE_VACANCY,
+        UPDATE_VACANCY,
+        DELETE_VACANCY,
+        ADD_EMPLOYEE_TO_INTERVIEW,
+        ADD_EMPLOYEE_TO_COMPANY,
+        CREATE_INTERVIEW,
+        UPDATE_INTERVIEW,
+        DELETE_INTERVIEW
+    ]
+
+class CompanyOwner(Hr):
+    permissions = Hr.permissions + [
+        CREATE_COMPANY,
+        UPDATE_COMPANY,
+        DELETE_COMPANY
+    ]
 
 class Role(models.Model):
     """ User's roles representation """
