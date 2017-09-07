89dd6cb9bbb0f394f51ec8de8a59390b83c05fea
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-07 23:30:24 +0300

    implement first background task about interview reminder sendings

--- app/interviews/emails.py
+++ app/interviews/emails.py
@@ -2,6 +2,8 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
 
+SENDER = 'Anymail Sender <from@example.com>'
+
 def send_interview_reminder(users, vacancy, interview):
     """ Sendm mail with remainding about the next interview """
 
@@ -10,4 +12,12 @@ def send_interview_reminder(users, vacancy, interview):
         'interview': interview
     }
     msg = render_to_string('interview_reminder.html', params)
-    send_mail("Reset password mail", msg, "Anymail Sender <from@example.com>", [user.email for user in users])
+    topic = 'Reset password mail'
+    emails = [user.email for user in users]
+    send_default_mail(topic, msg, SENDER, emails)
+
+
+def send_default_mail(topic, message, sender, mails):
+    """ Base mail send function """
+
+    send_mail(topic, message, sender, mails)
--- app/interviews/tasks.py
+++ app/interviews/tasks.py
@@ -1,8 +1,12 @@
 from celery import shared_task
+from .models import Interview
+from .emails import send_interview_reminder
 
 @shared_task
-def test_shared_task():
+def remind_about_interview():
     """ Test for the celery task """
 
-    print('TEST TASK IS RUNNING')
-    return 'TEST TASK'
+    for interview in Interview.in_range_of_days(15):
+        users = interview.interviewees.all()
+        vacancy = interview.vacancy
+        send_interview_reminder(users, vacancy, interview)
