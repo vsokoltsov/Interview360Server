d0229c29b06173e3a432a1319fead05d1c7a51fa
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-11 00:55:38 +0300

    add pg_hba.conf and and some other deploy configurations

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import cd, run, sudo, env
+from fabric.api import cd, run, sudo, env, put
 from fabric.contrib.files import exists
 
 PROJECT_NAME = 'interview_manager'
@@ -7,9 +7,19 @@ env.user = 'root'
 env.hosts = ['root@95.213.194.196']
 env.home_dir = '/root'
 
-def pull_remote():
+def pull_remote(branch):
     with cd(env.home_dir):
         if exists("{}/{}".format(env.home_dir, PROJECT_NAME)):
-            run('cd {}/{} && git pull && git checkout add-deploy'.format(env.home_dir, PROJECT_NAME))
+            run('cd {}/{} && git pull && git checkout {}'.format(env.home_dir, PROJECT_NAME, branch))
         else:
             run("git clone {} {}".format(GITHUB_PROJECT, PROJECT_NAME))
+
+def restart_gunicorn():
+    with cd(env.home_dir):
+        sudo('systemctl restart gunicorn', pty=False)
+        put('deploy/gunicorn.service', '{}'.format(env.home_dir))
+
+
+def deploy(branch='master'):
+    pull_remote(branch)
+    restart_gunicorn()
