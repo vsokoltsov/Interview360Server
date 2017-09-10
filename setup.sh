1b99ec5d84c1bae1920c2b142888058af3cc0ee4
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-11 00:46:36 +0300

    add nginx and gunicorn configuration

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ PYTHON_VERSION=3.5.2
 sudo yum -y update
 
 # PYTHON 3.5.2
-sudo yum install -y  gcc gcc-c++ make git patch openssl-devel zlib-devel readline-devel sqlite-devel bzip2-devel
+sudo yum install -y  gcc gcc-c++ make git patch openssl-devel zlib-devel readline-devel sqlite-devel bzip2-devel nginx
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
