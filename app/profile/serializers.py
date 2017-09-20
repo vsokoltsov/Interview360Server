f88fcdc33fe555f88ce8fb81556d6d15630145bb
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-21 00:12:05 +0300

    add update action to ProfileSerializer and ProfileViewSet

--- app/profile/serializers.py
+++ app/profile/serializers.py
@@ -2,4 +2,14 @@ from rest_framework import serializers
 from authorization.serializers import UserSerializer
 
 class ProfileSerializer(UserSerializer):
-    pass
+    email = serializers.EmailField(requred=True)
+    first_name = serializers.CharField(max_length=255, required=True)
+    last_name = serializers.CharField(max_length=255, required=True)
+
+    def update(self, instance, data):
+        """ Update existent profile """
+
+        instance.email = data.get('email', instance.email)
+        instance.first_name = data.get('first_name', instance.first_name)
+        instance.last_name = data.get('last_name', instance.last_name)
+        return instance
--- app/profile/views.py
+++ app/profile/views.py
@@ -16,3 +16,18 @@ class ProfileViewSet(viewsets.ViewSet):
 
         serializer = ProfileSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request):
+        """ Update current user information """
+
+        serializer = ProfileSerializer(
+            request.user, data=request.data, partial=True
+        )
+        if serializer.is_valid() and serializer.save():
+            return Response(
+                { 'current_user': serializer.data }, status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                { 'errors': serializer.errors }, status=status.HTTP_400_BAD_REQUEST
+            )
