c83882a903665b3c7155079ee7c0db73feec5571
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-21 16:57:49 +0300

    add some boilerplate for user permission for updating

---
+++ app/profile/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+
+class UserProfilePermission(permissions.BasePermission):
+    """ Permissions for accessing to user profile """
+
+    def has_permission(self, request, view):
+        return True
+
+
+    def has_object_permission(self, request, view, obj):
+        current_user = request.user
+        if view.action == 'retrieve':
+            return True
+        elif view.action == 'update' and current_user.id == user.id:
+            return True
+        elif view.action == 'destroy' and current_user.id == user.id:
+            return True
+        else:
+            return False
