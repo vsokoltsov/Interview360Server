9a02b771fcb54996ea658ae69b1035548667484a
Vadim Sokoltsov <vforvad@gmail.com>
2017-09-25 23:29:02 +0300

    add tests for user for saveing the attachment information

--- app/profile/tests.py
+++ app/profile/tests.py
@@ -1,6 +1,16 @@
+import app
+import os
+from os.path import abspath, join, dirname
+from tempfile import mkdtemp
+from django.core.files import File
 from rest_framework.test import APITestCase
 from authorization.models import User
 from rest_framework.authtoken.models import Token
+from attachments.models import Attachment
+from django.contrib.contenttypes.models import ContentType
+from django.test.utils import override_settings
+from django.core.files.storage import FileSystemStorage
+import mock
 import ipdb
 
 class ProfileViewSetTest(APITestCase):
@@ -11,6 +21,7 @@ class ProfileViewSetTest(APITestCase):
         password = 'aaaaaaaa'
         self.user = User.objects.create(email="example1@mail.com", password=password)
         self.token = Token.objects.create(user=self.user)
+        self.media_folder = mkdtemp()
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         self.form_data = {
             'email': 'awdawdwad@mail.ru',
@@ -22,6 +33,14 @@ class ProfileViewSetTest(APITestCase):
             'password': 'aaaaaa999999',
             'password_confirmation': 'aaaaaa999999'
         }
+
+    def tearDown(self):
+        """ Removing all dependencies after test """
+
+        dir_path = app.settings.MEDIA_ROOT
+        for f in os.listdir(dir_path):
+            file_path = "{}/{}".format(dir_path, f)
+            os.remove(file_path)
 
     def test_success_receiving_of_profile(self):
         """ Test success receiving current user information """
@@ -55,3 +74,22 @@ class ProfileViewSetTest(APITestCase):
             '/api/v1/users/{}/change_password/'.format(self.user.id), {}
         )
         assert 400, response.status_code
+
+    @mock.patch('storages.backends.s3boto3.S3Boto3Storage', FileSystemStorage)
+    def test_update_with_attachment_information(self):
+        """ Test update user instance with attahcment field """
+
+        content_type = ContentType.objects.get_for_model(User)
+        file_path = join(dirname(app.__file__), 'fixtures/test.jpg')
+
+        with open(file_path, 'rb') as photo:
+            attachment = Attachment(content_type=content_type)
+            attachment.data.save('test.jpg', File(photo), 'rb')
+            self.form_data['attachment'] = { 'id': attachment.id }
+
+            response = self.client.put(
+                '/api/v1/users/{}/'.format(self.user.id), self.form_data, format='json'
+            )
+
+            attachment.refresh_from_db()
+            assert attachment.object_id, self.user.id
