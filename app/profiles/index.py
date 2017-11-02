b6bf725c8a647f9c55508167757a65e0d79808c9
Vadim Sokoltsov <vforvad@gmail.com>
2017-11-03 00:23:56 +0300

    add user's avatars to index

--- app/profiles/index.py
+++ app/profiles/index.py
@@ -1,5 +1,5 @@
 from authorization.models import User
-from elasticsearch_dsl import DocType, Date, Integer, Keyword, Text
+from elasticsearch_dsl import DocType, Date, Integer, Keyword, Text, Object
 
 class UserIndex(DocType):
     """ User class index """
@@ -9,6 +9,7 @@ class UserIndex(DocType):
     last_name = Text(analyzer='snowball')
     email = Text(analyzer='snowball')
     company_ids = Integer()
+    attachment = Object()
 
     class Meta:
         index = 'users'
@@ -22,13 +23,15 @@ def rebuild_index():
         company_ids = list(
             map(lambda c: c.id, user.companies.all())
         )
+        attachment = user.attachments.last()
         obj = UserIndex(
             meta={'id': user.id},
             id=user.id,
             email=user.email,
             first_name=user.first_name,
             last_name=user.last_name,
-            company_ids=company_ids
+            company_ids=company_ids,
+            attachment=attachment.full_urls() if attachment else None
         )
         obj.save()
         print(obj.to_dict(include_meta=True))
