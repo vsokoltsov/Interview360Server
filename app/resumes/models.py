66adc66a1995ee435b1b36c39d30e710ef05e150
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-04 18:36:37 +0300

    add resume model and its migrations

--- app/resumes/models.py
+++ app/resumes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Resume(models.Model):
+    """ Resume representation in the system """
+
+    user = models.ForeignKey('authorization.User', null=False)
+    description = models.TextField()
+    skills = models.ManyToManyField('skills.Skill')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
