33cf8ce50d2d1c49c663f2ab09dba7d0f8b3f307
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-11 02:32:57 +0300

    add skill helper indexing

---
+++ common/indexes/skills.py
@@ -0,0 +1,18 @@
+from skills.models import Skill
+from skills.index import SkillIndex
+from elasticsearch_dsl import Index
+from elasticsearch.exceptions import TransportError
+import logging
+
+def rebuild_index():
+    """ Rebuild skills index """
+
+    try:
+        Index('skills').delete()
+    except TransportError:
+        logger.warning('There is no such index')
+    finally:
+        SkillIndex.init()
+        skills = Skill.objects.all()
+        for skill in skills:
+            print(SkillIndex.store_index(skill))
