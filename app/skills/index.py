9be0fdd28300f1c039ab211a8840678c979615b6
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-11 02:04:21 +0300

    add skills index class

---
+++ app/skills/index.py
@@ -0,0 +1,24 @@
+from elasticsearch_dsl import (
+    DocType, Integer, Text
+)
+
+class SkillIndex(DocType):
+    """ Skill's index class """
+
+    id = Integer()
+    name = Text(analyzer='standard')
+
+    class Meta:
+        index = 'companies'
+
+    @classmethod
+    def store_index(cls, skill):
+        """ Create or update skill's index """
+
+        obj = cls(
+            meta={'id': skill.id},
+            'id': skill.id,
+            'name': skill.name
+        )
+        obj.save()
+        return obj.to_dict(include_meta=True)
