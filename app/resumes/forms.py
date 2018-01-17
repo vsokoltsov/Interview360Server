9f210be667b625cbf68229c6cfe9c76a041190ac
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-18 00:53:46 +0300

    figured out how to create custom validation methods

--- app/resumes/forms.py
+++ app/resumes/forms.py
@@ -1,5 +1,15 @@
 from common.forms import BaseForm
+import cerberus
+from resumes.models import Resume, Workplace
+from companies.models import Company
 import ipdb
+
+def company_exist(field, value, error):
+    try:
+        company = Company.objects.get(name=value)
+    except Company.DoesNotExist:
+        error(field, 'Does not exist')
+
 
 class WorkplaceForm(BaseForm):
     """ Workplace form class """
@@ -25,7 +35,8 @@ class WorkplaceForm(BaseForm):
                         'type': 'integer'
                     },
                     'company': {
-                        'type': 'string'
+                        'type': 'string',
+                        'validator': company_exist
                     },
                     'description': {
                         'type': 'string'
@@ -43,7 +54,17 @@ class WorkplaceForm(BaseForm):
         }
     }
 
+    def company_exist(self, field, value, error):
+        """ Custom validation for the company parameter """
+        ipdb.set_trace()
+
     def submit(self):
         """ Check the from validation and create workplaces """
 
         if not self.is_valid(): return False
+
+        workplaces = self.params.get('workplaces')
+
+        for wp in workplaces:
+
+            workplace = Workplace.objects.create(**wp)
