d282aa509fe7c939e93eb76924483d8c32a496c2
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-18 00:57:41 +0300

    add presence validation to all workplace fields

--- app/resumes/forms.py
+++ app/resumes/forms.py
@@ -29,34 +29,40 @@ class WorkplaceForm(BaseForm):
                         'required': False
                     },
                     'position': {
-                        'type': 'string'
+                        'type': 'string',
+                        'empty': False,
+                        'required': True
                     },
                     'resume_id': {
-                        'type': 'integer'
+                        'type': 'integer',
+                        'empty': False,
+                        'required': True
                     },
                     'company': {
                         'type': 'string',
-                        'validator': company_exist
+                        'empty': False,
+                        'required': True
                     },
                     'description': {
-                        'type': 'string'
+                        'type': 'string',
+                        'empty': False,
+                        'required': True
                     },
                     'start_date': {
                         'type': 'string',
-                        'regex': '^\d{4}-\d{2}-\d{2}$'
+                        'regex': '^\d{4}-\d{2}-\d{2}$',
+                        'empty': False,
+                        'required': True
                     },
                     'end_date': {
                         'type': 'string',
-                        'regex': '^\d{4}-\d{2}-\d{2}$'
+                        'regex': '^\d{4}-\d{2}-\d{2}$',
+                        'required': True
                     }
                 }
             }
         }
     }
-
-    def company_exist(self, field, value, error):
-        """ Custom validation for the company parameter """
-        ipdb.set_trace()
 
     def submit(self):
         """ Check the from validation and create workplaces """
@@ -66,5 +72,6 @@ class WorkplaceForm(BaseForm):
         workplaces = self.params.get('workplaces')
 
         for wp in workplaces:
-
+            company_name = workplace.pop('company')
+            # company, created = Compan
             workplace = Workplace.objects.create(**wp)
