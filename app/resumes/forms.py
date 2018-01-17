1c0da9b6308122b19cedd9ce671ccfb8d202ddc6
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-18 00:24:06 +0300

    move base form into separated module

--- app/resumes/forms.py
+++ app/resumes/forms.py
@@ -1,57 +1,5 @@
-import abc
-import cerberus
-from cerberus import Validator
-from contextlib import contextmanager
-from django.db import transaction
-from django_pglocks import advisory_lock
+from common.forms import BaseForm
 import ipdb
-
-class BaseForm(abc.ABC):
-    """ Base form-object class """
-
-    @property
-    @abc.abstractmethod
-    def schema(self):
-        pass
-
-    def __init__(self, **kwargs):
-        """ Base class init method """
-
-        if not self.schema:
-            raise NotImplementedError('Subclasses must define schema property')
-
-        self.validator = Validator(self.schema)
-        self.obj = kwargs.get('obj', None)
-        self.params = kwargs.get('params', None)
-
-
-    def is_valid(self):
-        """ Return whether or not the receiving data are valid """
-
-        try:
-            result = self.validator.validate(self.params, self.schema)
-            if not result:
-                self.errors = self.validator.errors
-
-            return result
-        except cerberus.validator.DocumentError:
-            self.errors['base'] =  ['Invalid structure']
-            return False
-
-    @contextmanager
-    def submit(self, message=None):
-        """ Save object to the database """
-
-        if not self.is_valid(): return False
-
-        for key, value in self.params.items():
-            setattr(self.obj, key, value)
-
-        with transaction.atomic():
-            with advisory_lock(message if message else 'aaa'):
-                self.obj.save()
-                yield
-                return True
 
 class WorkplaceForm(BaseForm):
     """ Workplace form class """
@@ -94,7 +42,6 @@ class WorkplaceForm(BaseForm):
             }
         }
     }
-    errors = {}
 
     def submit(self):
         """ Check the from validation and create workplaces """
