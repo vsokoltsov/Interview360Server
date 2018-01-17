b616edae2a700a2f2552af5cd584b0b55a2010f0
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-17 23:58:31 +0300

    add abstract BaseForm class

--- app/resumes/forms.py
+++ app/resumes/forms.py
@@ -1,8 +1,62 @@
+import abc
 import cerberus
 from cerberus import Validator
-import ipdb
+from contextlib import contextmanager
+from django.db import transaction
+from django_pglocks import advisory_lock
 
-class WorkplaceForm:
+class BaseForm(abc.ABC):
+    """ Base form-object class """
+
+    @property
+    @abc.abstractmethod
+    def schema(self):
+        pass
+
+    @property
+    def params(self):
+        pass
+
+    def __init__(self, **kwargs):
+        """ Base class init method """
+
+        if not self.schema:
+            raise NotImplementedError('Subclasses must define schema property')
+
+        self.validator = Validator(self.schema)
+        self.obj = kwargs.get('obj', None)
+        self.params = kwargs.get('params', None)
+
+
+    def is_valid(self):
+        """ Return whether or not the receiving data are valid """
+
+        try:
+            result = self.validator.validate(self.params, self.schema)
+            if not result:
+                self.errors = self.validator.errors
+
+            return result
+        except cerberus.validator.DocumentError:
+            self.errors['base'] =  ['Invalid structure']
+            return False
+
+    @contextmanager
+    def submit(self, message=None):
+        """ Save object to the database """
+
+        if not self.is_valid(): return False
+
+        for key, value in self.params.items():
+            setattr(self.obj, key, value)
+
+        with transaction.atomic():
+            with advisory_lock(message if message else 'aaa'):
+                self.obj.save()
+                yield
+                return True
+
+class WorkplaceForm(BaseForm):
     """ Workplace form class """
 
     schema = {
@@ -24,26 +78,7 @@ class WorkplaceForm:
     }
     errors = {}
 
-    def __init__(self, params=None):
-        """ Base constructor; Create validator instance and assign parameters """
-
-        self.validator = Validator(self.schema)
-        self.params = params
-
     def submit(self):
         """ Check the from validation and create workplaces """
 
         if not self.is_valid(): return False
-
-    def is_valid(self):
-        """ Return whether or not the receiving data are valid """
-
-        try:
-            result = self.validator.validate(self.params, self.schema)
-            if not result:
-                self.errors = self.validator.errors
-
-            return result
-        except cerberus.validator.DocumentError:
-            self.errors['workplaces'] =  ['Invalid structure']
-            return False
