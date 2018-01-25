9657a86d23bf9393c14443b06dbd957bd1ddd389
Vadim Sokoltsov <vforvad@gmail.com>
2018-01-26 01:11:26 +0300

    change methods for elasticsearch's indexes rebuild

--- common/indexes/companies.py
+++ common/indexes/companies.py
@@ -1,12 +1,18 @@
 from companies.models import Company
 from companies.index import CompanyIndex
 from elasticsearch_dsl import Index
+from elasticsearch.exceptions import TransportError
+import logging
 
 def rebuild_company_index():
     """ Rebuild index for the companies """
 
-    Index('companies').delete()
-    CompanyIndex.init()
-    companies = Company.objects.all()
-    for company in companies:
-        print(CompanyIndex.store_index(company))
+    try:
+        Index('companies').delete()
+    except TransportError:
+        loggin.logger.warning('There is no such index')
+    finally:
+        CompanyIndex.init()
+        companies = Company.objects.all()
+        for company in companies:
+            print(CompanyIndex.store_index(company))
