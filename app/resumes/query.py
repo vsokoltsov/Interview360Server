01b5eaea0c385eb6c588a0990b3497802fdbdc47
Vadim Sokoltsov <vforvad@gmail.com>
2018-02-19 23:49:42 +0300

    add properties for the ResumeQuery class

--- app/resumes/query.py
+++ app/resumes/query.py
@@ -1,3 +1,5 @@
+import re
+
 from django.db.models import Count
 from resumes.models import Resume
 import ipdb
@@ -5,12 +7,18 @@ import ipdb
 class ResumesQuery:
     """ Advanced Query class for the Resume """
 
-    def __init__(self, params=None):
+    VALID_ORDER_FIELDS = [
+        'title',
+        'salary',
+        'created_at',
+        'updated_at',
+        'workplaces_count'
+    ]
+
+    def __init__(self, params):
         """ Constructor; Set parameters instead of default ones """
 
-        self.order = params.get('order')
-        self.salary = params.get('salary')
-        self.skills = params.get('skills')
+        self.params = params
 
 
     def list(self):
@@ -23,3 +31,29 @@ class ResumesQuery:
         if self.order:
             queryset = queryset.order_by(self.order)
         return queryset
+
+    @property
+    def salary(self):
+        """ Return salary value """
+
+        return self.params.get('salary')
+
+    @property
+    def order(self):
+        """ Retur order value """
+
+        return order if self.is_valid_order(order) else None
+
+    @property
+    def skills(self):
+        """ Return skills value """
+
+        return self.params.get('skills')
+
+    def is_valid_order(self, order):
+        """ Return whether or not the order value is valid """
+
+        order_template = re.compile('-')
+        if order_template.match(str(order)):
+            order = order[1:]
+        return order in self.VALID_ORDER_FIELDS
