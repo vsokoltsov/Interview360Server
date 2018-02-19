b0774936abb3165d14e8e92f3dd9a784dca4367f
Vadim Sokoltsov <vforvad@gmail.com>
2018-02-19 19:26:05 +0300

    set query class to the resumes views

--- app/resumes/query.py
+++ app/resumes/query.py
@@ -5,9 +5,17 @@ class ResumesQuery:
 
     def __init__(self, params=None):
         """ Constructor; Set parameters instead of default ones """
-        pass
 
-    @classmethod
-    def list():
+        self.order = params.get('order')
+        self.salary = params.get('salary')
+        self.skills = params.get('skills')
+
+    def list(self):
         """ Perform query for extracting the list of the Resume instances """
-        pass
+
+        queryset = Resume.objects.prefetch_related(
+            'user', 'user__avatars'
+        )
+        if self.order:
+            queryset = queryset.order_by(self.order)
+        return queryset
