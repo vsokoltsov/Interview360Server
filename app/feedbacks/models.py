d5de933988383b89a9495025c5562e8493f677b0
Vadim Sokoltsov <vforvad@gmail.com>
2018-03-07 01:31:17 +0300

    add feedbacks statuses

--- app/feedbacks/models.py
+++ app/feedbacks/models.py
@@ -6,6 +6,22 @@ from authorization.models import User
 class Feedback(models.Model):
     """ Feedback model representations """
 
+    ASSIGNED = 0
+    IN_PROGRESS = 1
+    DONE = 2
+
+    STATUSES = [
+        ASSIGNED,
+        IN_PROGRESS,
+        DONE
+    ]
+
+    STATUSES_NAMES = [
+        'assigned',
+        'in progress',
+        'done'
+    ]
+
     user = models.ForeignKey(User, null=False)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,3 +31,9 @@ class Feedback(models.Model):
 
     class Meta:
         db_table = 'feedbacks'
+
+    @classmethod
+    def status_value(cls, val):
+        """ Get name of the status by status value """
+
+        return cls.STATUSES_NAMES.index(val) if val in cls.STATUSES else None
