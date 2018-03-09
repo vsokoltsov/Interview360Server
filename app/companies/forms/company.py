5a46fa338881cfe2b6a6338eae60ec32fac7b4be
Vadim Sokoltsov <vforvad@gmail.com>
2018-03-09 23:53:56 +0300

    add boilerplate for the form of the company object

---
+++ app/companies/forms/company.py
@@ -0,0 +1,65 @@
+from common.forms import BaseForm
+from django.db import transaction
+from authorization.models import User
+
+def owner_exist(field, value, error):
+    """ Check whether or not owner exist """
+
+    try:
+        user = User.objects.get(id=value)
+    except User.DoesNotExist:
+        error(field, 'Does not exist')
+
+class CompanyForm(BaseForm):
+    """ Form object for company model """
+
+    schema = {
+        'id': {
+            'type': 'integer',
+            'required': False
+        },
+        'name': {
+            'type': 'string',
+            'empty': False,
+            'required': True
+        },
+        'start_date': {
+            'type': 'string',
+            'regex': '^\d{4}-\d{2}-\d{2}$',
+            'empty': False,
+            'required': True
+        },
+        'description': {
+            'type': 'string',
+            'empty': False,
+            'required': True
+        },
+        'city': {
+            'type': 'string',
+            'empty': False,
+            'required': True
+        },
+        'owner_id': {
+            'type': 'integer',
+            'empty': False,
+            'required': True,
+            'validator': owner_exist
+        },
+    }
+
+    def submit(self):
+        """ Set attributes to the company instance and save to the database """
+
+        if not self.is_valid(): return False
+
+        try:
+            with transaction.atomic():
+                self.__set_attributes()
+                self.obj.save()
+        except:
+            return False
+
+
+    def __set_attributes(self):
+        for field, value in self.params.items():
+            setattr(self.obj, field, value)
