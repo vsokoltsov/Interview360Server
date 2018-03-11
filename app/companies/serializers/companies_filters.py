1b0d7e4bfafadf81c2c80c74679e8c3a080ce9b1
Vadim Sokoltsov <vforvad@gmail.com>
2018-03-12 00:56:02 +0300

    add companies filters class

--- app/companies/serializers/companies_filters.py
+++ app/companies/serializers/companies_filters.py
@@ -1,5 +1,28 @@
-from . import serializers, Company
+from . import serializers, Company, CompanyMember
 
 class CompaniesFilter(serializers.Serializer):
     """ Serializer for companies filters data """
-    pass
+
+    roles = serializers.SerializerMethodField()
+    order = serializers.SerializerMethodField()
+
+    ORDERS = (
+        ('employees_count', 'Employees count'),
+        ('interviews_count', 'Interviews count'),
+        ('vacancies_count', 'Vacancies count')
+    )
+
+    def get_roles(self, obj):
+        """ Receive dict of the roles """
+
+        return [
+            {'title': val, 'key': key } for key, val in CompanyMember.ROLES
+        ]
+
+    def get_order(self, obj):
+        """ Receive of the possible orders """
+
+
+        return [
+            {'title': val, 'key': key } for key, val in self.ORDERS
+        ]
