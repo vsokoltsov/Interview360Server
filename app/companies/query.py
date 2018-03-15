77d021aa24867c976ce7000090a1736ee2c83342
Vadim Sokoltsov <vforvad@gmail.com>
2018-03-15 20:52:48 +0300

    add bolerplate for the company's query object

--- app/companies/query.py
+++ app/companies/query.py
@@ -1,11 +1,14 @@
 import re
 from decimal import Decimal
 
+from common.queries import BaseQuery, QueryOrderMixin
 from django.db.models import Count
 from companies.models import Company, CompanyMember
 
-class CompaniesQuery:
+class CompaniesQuery(BaseQuery, QueryOrderMixin):
     """ Query class for Company """
+
+    order_fields = [k for k, _ in Company.ORDER_FIELDS]
 
     def __init__(self, params, current_user):
         """ Constructor; Set parameters and current user """
@@ -17,14 +20,32 @@ class CompaniesQuery:
         """ Perform query for extracting list of the Company instances """
 
         queryset = self.current_user.companies.prefetched_list()
+
+        if self.role:
+            queryset = queryset.filter(
+                companymember__user_id=self.current_user.id,
+                companymember__role=self.role
+            )
+
+        if self.order:
+            queryset = queryset.order_by(self.order)
+
         return queryset
 
     @property
     def role(self):
         """ Return value of the role """
-        pass
+
+        role = self.params.get('role')
+        return role if self._is_valid_role(role) else None
 
     @property
-    def order(self):
-        """ Return value of the order """
-        pass
+    def roles(self):
+        """ Return list of roles for the validation of role """
+
+        return [k for k, _ in CompanyMember.ROLES]
+
+    def _is_valid_role(self, role):
+        """ Check whether or not the role is valid """
+
+        return role in self.roles
