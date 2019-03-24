45816b824f918ec1b18a02aec7d60b9b1b2440dd
Vadim Sokoltsov <vforvad@gmail.com>
2019-03-24 20:41:49 +0300

    Change the way of structuring the logs

--- app/app/logging/production.py
+++ app/app/logging/production.py
@@ -1,54 +1,29 @@
 import sys
 import logging
+from .default import LOGGING
 from boto3.session import Session
 
 from app.credentials import (
     AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION_NAME
 )
 
-boto3_session = Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
-                        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                        region_name=AWS_REGION_NAME)
+if AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID:
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'root': {
-        'level': logging.ERROR,
-        'handlers': ['console'],
-    },
-    'formatters': {
-        'simple': {
-            'format': u"%(asctime)s [%(levelname)-8s] %(message)s",
-            'datefmt': "%Y-%m-%d %H:%M:%S"
-        },
-        'aws': {
-            'format': u"%(asctime)s [%(levelname)-8s] %(message)s",
-            'datefmt': "%Y-%m-%d %H:%M:%S"
-        },
-    },
-
-    'handlers': {
-        'watchtower': {
-            'level': 'DEBUG',
-            'class': 'watchtower.CloudWatchLogHandler',
-                     'boto3_session': boto3_session,
-                     'log_group': 'MyLogGroupName',
+    boto3_session = Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
+                            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                            region_name=AWS_REGION_NAME)
+    aws_format = {
+        'format': u"%(asctime)s [%(levelname)-8s] %(message)s",
+        'datefmt': "%Y-%m-%d %H:%M:%S"
+    }
+    watchtower_handler = {
+        'level': 'DEBUG',
+        'class': 'watchtower.CloudWatchLogHandler',
+        'boto3_session': boto3_session,
+        'log_group': 'MyLogGroupName',
                      'stream_name': 'MyStreamName',
-            'formatter': 'aws',
-        },
-        'console': {
-            'level': logging.DEBUG,
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
-            'stream': sys.stdout
-        }
-    },
-    'loggers': {
-        'django': {
-            'level': 'INFO',
-            'handlers': ['watchtower'],
-            'propagate': False,
-        },
-    },
-}
+        'formatter': 'aws',
+    }
+    LOGGING['formatters']['aws'] = aws_format
+    LOGGING['handlers']['watchtower'] = watchtower_handler
+    LOGGING['loggers']['django']['handlers'] += ['watchtower']
