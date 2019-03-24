91ba6dffdd37c7b79a1d775ebec43c95866cf124
Vadim Sokoltsov <vforvad@gmail.com>
2019-03-24 14:25:46 +0300

    Move credentials into separated package

---
+++ app/app/credentials/aws.py
@@ -0,0 +1,6 @@
+import os
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET')
+AWS_REGION_NAME = os.environ.get('AWS_REGION')
