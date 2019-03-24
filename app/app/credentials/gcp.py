78c314caddb3d13dc31ac57bbca11a4855e77582
Vadim Sokoltsov <vforvad@gmail.com>
2019-03-24 20:03:37 +0300

    Remove useless credentials

--- app/app/credentials/gcp.py
+++ app/app/credentials/gcp.py
@@ -1,7 +1,4 @@
 import os
 
-GCP_ACCESS_KEY = os.environ.get('GCP_ACCESS_KEY')
-GCP_ACCESS_SECRET = os.environ.get('GCP_ACCESS_SECRET')
 GS_BUCKET_NAME = os.environ.get('GCP_BUCKET_NAME')
-GS_PROJECT_ID = 'interview360manager'
 GCP_CREDENTIALS_NAME = os.environ.get('GCP_CREDENTIALS_NAME')
