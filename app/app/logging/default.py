067680068af82272fe532d102572a47d6c28fe30
Vadim Sokoltsov <vforvad@gmail.com>
2019-03-25 15:20:47 +0300

    Implement logstash logging

--- app/app/logging/default.py
+++ app/app/logging/default.py
@@ -4,6 +4,7 @@ from app.paths import BASE_DIR
 
 HANDLERS_LIST = ['console']
 DISABLE_FILE_LOGGING = os.environ.get('DISABLE_FILE_LOGGING', False)
+ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
 
 CONSOLE_HANDLER = {
     'level': logging.DEBUG,
@@ -45,3 +46,17 @@ if not DISABLE_FILE_LOGGING:
     }
     LOGGING['handlers']['file'] = FILE_HANDLER
     LOGGING['loggers']['django']['handlers'] += ['file']
+
+if ELASTICSEARCH_URL:
+    logstash_handler = {
+        'level': 'DEBUG',
+        'class': 'logstash.TCPLogstashHandler',
+        'host': 'logstash',
+        'port': 5000,
+        'version': 1,
+        'message_type': 'logstash',
+        'fqdn': True,  # Fully qualified domain name. Default value: false.
+        'tags': ['interview360', 'django'],
+    }
+    LOGGING['handlers']['logstash'] = logstash_handler
+    LOGGING['loggers']['django']['handlers'] += ['logstash']
